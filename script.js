f3c53a1ad38dbf2bf7457f2091a934f1c3837d8e
Abu Shahen <ashahen@bmc.com>
2025-04-12 11:26:11 +0300

    Added HTML, CSS, JS files with team info and Bootstrap design

---
+++ script.js
@@ -0,0 +1,6 @@
+// A simple JavaScript action: say hello when the button is clicked
+function sayHello() {
+    const message = "Hello from Ahed Abu Shahen!";
+    document.getElementById("helloMsg").textContent = message;
+  }
+  
